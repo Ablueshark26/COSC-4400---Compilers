565697a9a7b0d01a9900e1daf77888869083129c
Samuel Biskupic <Samuel.biskupic@marquette.edu>
2026-09-09 12:27:43 -0500

    we ball

--- Scanner.java
+++ Scanner.java
@@ -298,13 +298,13 @@ public String getToken (java.io.Reader reader) throws java.io.IOException
 				int numErr = 0;
 				for (int i = 0; i < lexeme.length(); i++){
 					if(!Character.isDigit(lexeme.charAt(i))){
-						if(lexeme.charAt(0) == '0'){
-							numErr = 2;
-						}
-						else if(lexeme.substring(0,2) == "0x"){
+						if(lexeme.substring(0,2) == "0x"){
 							numErr = 1;
 						}
-						else{
+						else if(lexeme.charAt(0) == '0'){
+							numErr = 2;
+						}
+						else if(lexeme.charAt(0) != '0'){
 							numErr = 3;
 						}
 					}
